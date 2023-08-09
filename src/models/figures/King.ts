34f5b3ea68c3d3e813c0efddc5a8e553ec37c35f
olegburevestnik <olegburevestnik@gmail.com>
2023-08-09 17:02:08 -0500

    First version according to https://www.youtube.com/watch?v=mUvYGUYMvKo

---
+++ src/models/figures/King.ts
@@ -0,0 +1,25 @@
+import {Figure, FigureNames} from "./Figure"
+import {Colors} from "../Colors";
+import {Cell} from "../Cell";
+import blackLogo from "../../assets/black-king.png";
+import whiteLogo from "../../assets/white-king.png";
+
+export class King extends Figure {
+
+    constructor(color: Colors, cell: Cell) {
+        super(color, cell);
+        this.logo = color === Colors.BLACK ? blackLogo : whiteLogo;
+        this.name = FigureNames.KING;
+    }
+
+    canMove(target: Cell): boolean {
+        if (!super.canMove(target))
+            return false;
+
+        if (Math.abs(target.x - this.cell.x) <= 1
+            && Math.abs(target.y - this.cell.y) <= 1)
+            return true;
+
+        return false;
+    }
+}
